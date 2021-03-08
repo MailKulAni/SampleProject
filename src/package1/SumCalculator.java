5a15018b9c7306203df85fd7aff66f06bae44ea5
Neha Kulkarni <nehakulkarni@Nehas-MacBook-Pro.local>
2021-03-08 18:25:32 -0500

    first commit

---
+++ src/package1/SumCalculator.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class SumCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int sum = calculateSum(1,2);
+		System.out.println("sum is: "+sum);
+	}
+	
+	public static int calculateSum(int a, int b) {
+		return a+b;
+	}
+}
